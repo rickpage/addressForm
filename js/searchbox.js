4b3afee636f202fa8c72d196f9ad95f113ba5017
Page <rickpagersc@gmail.com>
2016-08-17 01:49:03 -0400

    better comments

--- js/searchbox.js
+++ js/searchbox.js
@@ -1,15 +1,31 @@
 /*
- * Searchbox functionality
+ * Searchbox functionality to assist in searching and
+ * selecting a desired address for use in JSON.
+ * 
  * Usage:
- * call initAutocomplete from google api link i.e.
+ * 1. Write a function such as initAutocomplete that calls
+function initAutocomplete(){
+initializeSearchbox(
+  { lat : starting_lat
+  , lng : starting_lng
+  , callback : fill_in_form_address_data }
+ )
+ ...
+ }
+ *
+ * 2. call initAutocomplete from google api link i.e.
  * <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWNNCsmSfb0gpZv3IuK1c91cctc2y6FYY&libraries=places&amp;callback=initAutocomplete"
          async defer
- * Pre-requisites:
- *          
-        form id="theForm" onsubmit="return false;"
-        
-        Also need elements with the following ids (optional)
- * TODO
+ *
+ * 3. When user searches and/or clicks an icon, the
+ * script stores that address data and fires a callback.
+ * The default callback is:
+ * 
+ * fillInAddress(p)
+ * 
+ * p - place data
+ * 
+ * TODO: Explain prereqs for this to work out of box
  */
 var addressData = {};
 var _defaultLatLon = {lat: -33.8688, lng: 151.2195};
@@ -231,8 +247,11 @@ function init(map, callback) {
         
       }
       /*
+       * Fill In address (by element ID)
        * Take place details and fill in the form
        * elements with the values
+       * If the value is undefined, 'undefined' is
+       * used as the innerHTML or value
        */
       function fillInAddress(place,status) {
         var e = null;
